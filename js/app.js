212c80ae42ac1a0a6981ce4829d30bac609c9524
kevin chao <kchao562@gmail.com>
2017-08-15 22:36:52 -0700

    added date

--- js/app.js
+++ js/app.js
@@ -15,18 +15,24 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 $('.button').on('click',function(){
-  database.ref('users/').child($(this).parent().attr('id')).set($(this).html());
+  database.ref('users/').child($(this).parent().attr('id')).child('color').set($(this).html());
+  database.ref('users/').child($(this).parent().attr('id')).child('date').set(moment().format("dddd, MMMM Do YYYY, h:mm a"));
 })
 
 // Listener
 database.ref().on('value',function(snapshot){
   var snap = snapshot.val();
-  var user1color = snap.users.user1;
-  var user2color = snap.users.user2;
+  var user1color = snap.users.user1.color;
+  var user1date  = snap.users.user1.date;
+  var user2color = snap.users.user2.color;
+  var user2date  = snap.users.user2.date;
+
   $('#user1shirt').css('background-color', user1color.toLowerCase());
-  $('#user2shirt').css('background-color',user2color.toLowerCase());
+  $('#user2shirt').css('background-color', user2color.toLowerCase());
   $('#user1shirt').text('Shirt Color: '+user1color);
   $('#user2shirt').text('Shirt Color: '+user2color);
+  $('#user1date').text('Last Updated: '+user1date);
+  $('#user2date').text('Last Updated: '+user2date);
   if(user1color === 'White'){
     $('#user1shirt').css('color', 'black');
   }
